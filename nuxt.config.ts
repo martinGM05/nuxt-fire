aac195956088d24945fee679f9266deb2c06fea7
Martin <mgonzalez@mediomelon.mx>
2024-03-18 10:54:03 -0600

    Re design architec for firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
+  srcDir: "src/",
   runtimeConfig: {
     apiSecret: "123",
 
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useFirebaseAuth } from "~/composables/useFirebaseAuth";
+import { useFirebaseAuth } from "../composables/useFirebaseAuth";
 import { z } from "zod";
 import type { FormSubmitEvent } from "#ui/types";
 
